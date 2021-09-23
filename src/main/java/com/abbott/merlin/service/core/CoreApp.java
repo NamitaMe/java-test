feef2b38dc6f0d60ef31b8525b5cff4e348f244b
NamitaMe <88753484+NamitaMe@users.noreply.github.com>
2021-09-22 22:10:00 -0700

    Update CoreApp.java

--- src/main/java/com/abbott/merlin/service/core/CoreApp.java
+++ src/main/java/com/abbott/merlin/service/core/CoreApp.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class CoreApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApp.class, args)
+		SpringApplication.run(CoreApp.class, args);
 	}
 
 	
